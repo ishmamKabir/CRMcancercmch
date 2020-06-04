b30b876a43b445f64ca3b61184c86639b17305fb
ishmamKabir <kabirishmam@gmail.com>
2020-06-04 14:33:54 +0600

    Login

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -31,62 +31,21 @@ export const logout = () => {
     }
 }
 
+/*export const authKeepLogin = (userName, password) => {
+    return dispatch => {
+        if (userName === 'Ishmam' && password === '12345'){
+            dispatch(authSuccess(1));
+            localStorage.setItem('token', 1);
+            localStorage.setItem('userName', userName);
+            localStorage.setItem('password', password);
+            history.push('/');
+        }else{
+            dispatch(authFail('Invalid username or password'));
+        } 
+    }
+}*/
+
 export const authKeepLogin = (userName, password) => {
-    return dispatch => {
-        if (userName === 'Ishmam' && password === '12345'){
-            dispatch(authSuccess(1));
-            localStorage.setItem('token', 1);
-            localStorage.setItem('userName', userName);
-            localStorage.setItem('password', password);
-            history.push('/');
-        }else{
-            dispatch(authFail('Invalid username or password'));
-        } 
-    }
-}
-
-export const authLogin = (userName, password) => {
-    return dispatch => {
-        if (userName === 'Ishmam' && password === '12345'){
-            dispatch(authSuccess(1));
-            history.push('/');
-        }else{
-            dispatch(authFail('Invalid username or password'));
-        }    
-    }
-}
-
-export const authIsLoggedIn = (userName, password) => {
-    return dispatch => {
-        if (userName === 'Ishmam' && password === '12345'){
-            dispatch(authSuccess(1));
-            localStorage.setItem('token', 1);
-            localStorage.setItem('userName', userName);
-            localStorage.setItem('password', password);
-            
-        }else{
-            dispatch(authFail('Invalid username or password'));
-        } 
-    }
-}
-
-export const authCheckState = () => {
-    return dispatch => {
-        const token = localStorage.getItem('token');
-        const userName = localStorage.getItem('userName');
-        const password = localStorage.getItem('password');
-        if (token === null) {
-            dispatch(logout());
-        } else {
-            dispatch(authIsLoggedIn(userName,password));
-        }
-    
-    }
-}
-
-
-
-/*export const authLogin = (userName, password) => {
     return dispatch => {
         dispatch(authStart());
         post('/rest-auth/login', {
@@ -106,4 +65,84 @@ export const authCheckState = () => {
         })
     }
 
+}
+
+/*export const authLogin = (userName, password) => {
+    return dispatch => {
+        if (userName === 'Ishmam' && password === '12345'){
+            dispatch(authSuccess(1));
+            history.push('/');
+        }else{
+            dispatch(authFail('Invalid username or password'));
+        }    
+    }
 }*/
+
+/*export const authIsLoggedIn = (userName, password) => {
+    return dispatch => {
+        if (userName === 'Ishmam' && password === '12345'){
+            dispatch(authSuccess(1));
+            localStorage.setItem('token', 1);
+            localStorage.setItem('userName', userName);
+            localStorage.setItem('password', password);
+            
+        }else{
+            dispatch(authFail('Invalid username or password'));
+        } 
+    }
+}*/
+
+export const authIsLoggedIn = (userName, password) => {
+    return dispatch => {
+        dispatch(authStart());
+        post('/rest-auth/login', {
+            username: userName,
+            password: password
+        })
+        .then(res=> {
+            const token = res.data.key;
+            localStorage.setItem('token', token);
+            localStorage.setItem('userName', userName);
+            localStorage.setItem('password', password);
+            dispatch(authSuccess(token));
+            history.push('/');
+        })
+        .catch(err => {
+            dispatch(authFail(err))
+        })
+    }
+}
+
+export const authCheckState = () => {
+    return dispatch => {
+        const token = localStorage.getItem('token');
+        const userName = localStorage.getItem('userName');
+        const password = localStorage.getItem('password');
+        if (token === null) {
+            dispatch(logout());
+        } else {
+            dispatch(authIsLoggedIn(userName,password));
+        }
+    
+    }
+}
+
+
+export const authLogin = (userName, password) => {
+    return dispatch => {
+        dispatch(authStart());
+        post('/rest-auth/login', {
+            username: userName,
+            password: password
+        })
+        .then(res=> {
+            const token = res.data.key;
+            dispatch(authSuccess(token));
+            history.push('/');
+        })
+        .catch(err => {
+            dispatch(authFail(err))
+        })
+    }
+
+}
